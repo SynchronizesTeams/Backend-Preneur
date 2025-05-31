126a75bad898b4a34f721e1bbf52d8034743fffc
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-05-31 19:21:15 +0700

    namabahin seeder

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\StudentProfile;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        StudentProfile::insert([
+            'siswa_id' => uniqid("siswa_"),
+            'nama' => "Adly Fahreza",
+            'nis' => 232410003
+        ]);
+
+        StudentProfile::insert([
+            'siswa_id' => uniqid("siswa_"),
+            'nama' => "Gheraldy Moses Tarigan",
+            'nis' => 232410024
+        ]);
     }
 }
